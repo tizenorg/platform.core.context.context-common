b44168dfad452336c00a8588ea975ce0066ff735
Mu-Woong Lee <muwoong.lee@samsung.com>
2016-02-24 14:47:43 +0900

    Re-implement ctx::TimerManager class
    Change-Id: I70807e42c04561b6924b44bbbbfc8463ec6930dd
    Signed-off-by: Mu-Woong Lee <muwoong.lee@samsung.com>

--- include/TimerManager.h
+++ include/TimerManager.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 Samsung Electronics Co., Ltd.
+ * Copyright (c) 2016 Samsung Electronics Co., Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,16 +14,49 @@
  * limitations under the License.
  */
 
-#ifndef __CONTEXT_TIMER_MANAGER_H__
-#define __CONTEXT_TIMER_MANAGER_H__
+#ifndef _CONTEXT_TIMER_H_
+#define _CONTEXT_TIMER_H_
 
-#include <timer_types.h>
+#include <glib.h>
+#include <string>
+#include <set>
+#include <map>
+#include <ITimerListener.h>
+
+#define DAY_PER_WEEK	7
+#define DOW_MON		"Mon"
+#define DOW_TUE		"Tue"
+#define DOW_WED		"Wed"
+#define DOW_THU		"Thu"
+#define DOW_FRI		"Fri"
+#define DOW_SAT		"Sat"
+#define DOW_SUN		"Sun"
+#define DOW_WEEKDAY	"Weekday"
+#define DOW_WEEKEND	"Weekend"
+#define DOW_EVERYDAY	"Everyday"
 
 namespace ctx {
-	/* Forward Declaration */
-	class timer_listener_iface;
 
-	namespace timer_manager {
+	enum class DayOfWeek {
+		SUN = 0x01,
+		MON = 0x02,
+		TUE = 0x04,
+		WED = 0x08,
+		THU = 0x10,
+		FRI = 0x20,
+		SAT = 0x40,
+		WEEKDAY = MON | TUE | WED | THU | FRI,
+		WEEKEND = SAT | SUN,
+		EVERYDAY = SUN | MON | TUE | WED | THU | FRI | SAT
+	};
+
+	class TimerManager {
+	public:
+		TimerManager();
+		~TimerManager();
+
+		static std::string dowToStr(int dow);
+		static int dowToInt(std::string dow);
 
 		/**
 		 * @brief		Sets a repeated timer for a given interval of time (s).
@@ -34,7 +67,7 @@ namespace ctx {
 		 * @param[in]	listener	A listner object to be notified.
 		 * @return		Timer ID. A negative integer if failed to set a timer.
 		 */
-		int set_for(int interval, timer_listener_iface* listener, void* user_data = NULL);
+		int setFor(int interval, ITimerListener *listener);
 
 		/**
 		 * @brief		Sets a timer that will be expired at a specific time, at designated days of week.
@@ -42,17 +75,30 @@ namespace ctx {
 		 *				If it is possible to share a timer for multiple purposes, please do that.
 		 * @param[in]	hour		Hour
 		 * @param[in]	min			Minute
-		 * @param[in]	day_of_week	The timer will expire at hour:min:00, at every day(s) of week designated here.
+		 * @param[in]	dow			The timer will expire at hour:min:00, at every day(s) of week designated here.
 		 * @param[in]	listener	Listener object.
 		 * @return		Timer ID. A negative integer if failed to set a timer.
 		 */
-		int set_at(int hour, int min, int day_of_week, timer_listener_iface* listener, void* user_data = NULL);
+		int setAt(int hour, int min, DayOfWeek dow, ITimerListener *listener);
 
 		/**
-		 * @brief		Removes the timer specified by @c timer_id.
+		 * @brief		Removes the timer specified by @c timerId.
 		 */
-		void remove(int timer_id);
-	}
+		void remove(int timerId);
+
+	private:
+		static int __onAlarmExpired(int alarmId, void *userdata);
+
+		bool __init();
+		void __release();
+
+		static unsigned int __instanceCnt;
+		static GMutex __mutex;
+		static std::map<int, std::pair<ITimerListener*, TimerManager*>> __listenerMap;
+
+		std::set<int> __alarms;
+	};
+
 }	/* namespace ctx */
 
-#endif	/* __CONTEXT_TIMER_MANAGER_H__ */
+#endif	/* _CONTEXT_TIMER_H_ */
