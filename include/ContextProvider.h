ba356dac78ff88ce2910dbae9016e9234afbe60b
Mu-Woong Lee <muwoong.lee@samsung.com>
2016-04-18 12:08:23 +0900

    Cleanup ContextProvider and ContextManager interfaces
    Change-Id: I2395ed984f7b21a67b324c9f4472a8ea7a893906
    Signed-off-by: Mu-Woong Lee <muwoong.lee@samsung.com>

---
+++ include/ContextProvider.h
@@ -0,0 +1,65 @@
+/*
+ * Copyright (c) 2015 Samsung Electronics Co., Ltd.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef _CONTEXT_CONTEXT_PROVIDER_H_
+#define _CONTEXT_CONTEXT_PROVIDER_H_
+
+#include <Types.h>
+#include <Json.h>
+
+#define OPS_SUBSCRIBE	1
+#define OPS_READ		2
+#define OPS_WRITE		4
+
+namespace ctx {
+
+	class IContextManager;
+
+	class SO_EXPORT ContextProvider {
+	public:
+		virtual ~ContextProvider();
+
+		virtual bool isSupported();
+		virtual int subscribe(Json option, Json *requestResult);
+		virtual int unsubscribe(Json option);
+		virtual int read(Json option, Json *requestResult);
+		virtual int write(Json data, Json *requestResult);
+
+		/* TODO: Most of these functions will be removed later */
+		bool registerProvider(const char *privilege, ContextProvider *provider);
+		bool unregisterProvider();
+		bool registerTriggerItem(int operation, Json attributes, Json options, const char* owner = NULL);
+		bool unregisterTriggerItem();
+		bool publish(Json option, int error, Json dataUpdated);
+		bool replyToRead(Json option, int error, Json dataRead);
+
+	protected:
+		ContextProvider(const char *subject);
+
+	private:
+		static void __setContextManager(IContextManager *contextMgr);
+
+		static IContextManager *__contextMgr;
+
+		const char *__subject;
+
+		friend class ContextManager;
+
+	};	/* class ContextProvider */
+
+}	/* namespace ctx */
+
+#endif	/* _CONTEXT_CONTEXT_PROVIDER_H_ */
--- include/IContextManager.h
+++ include/IContextManager.h
@@ -14,32 +14,27 @@
  * limitations under the License.
  */
 
-#ifndef _CONTEXT_MANAGER_INTERFACE_H_
-#define _CONTEXT_MANAGER_INTERFACE_H_
+#ifndef _CONTEXT_I_CONTEXT_MANAGER_H_
+#define _CONTEXT_I_CONTEXT_MANAGER_H_
 
 #include <Types.h>
+#include <Json.h>
 
 namespace ctx {
-	/* Forward Declaration */
-	class Json;
-	class ContextProviderBase;
-	class ContextProviderInfo;
+
+	class ContextProvider;
 
 	class SO_EXPORT IContextManager {
 	public:
 		virtual ~IContextManager();
-		virtual bool registerProvider(const char *subject, ContextProviderInfo &providerInfo) = 0;
+		virtual bool registerProvider(const char *subject, const char *privilege, ContextProvider *provider) = 0;
 		virtual bool unregisterProvider(const char *subject) = 0;
-		virtual bool registerTriggerItem(const char *subject, int operation, ctx::Json attributes, ctx::Json options, const char* owner = NULL) = 0;
+		virtual bool registerTriggerItem(const char *subject, int operation, Json attributes, Json options, const char *owner) = 0;
 		virtual bool unregisterTriggerItem(const char *subject) = 0;
-		virtual bool publish(const char *subject, ctx::Json &option, int error, ctx::Json &dataUpdated) = 0;
-		virtual bool replyToRead(const char *subject, ctx::Json &option, int error, ctx::Json &dataRead) = 0;
+		virtual bool publish(const char *subject, Json &option, int error, Json &dataUpdated) = 0;
+		virtual bool replyToRead(const char *subject, Json &option, int error, Json &dataRead) = 0;
 	};	/* class IContextManager */
-
-	namespace context_manager {
-		void setInstance(IContextManager* mgr);
-	}	/* namespace context_manager */
 
 }	/* namespace ctx */
 
-#endif	/* End of _CONTEXT_MANAGER_INTERFACE_H_ */
+#endif	/* _CONTEXT_I_CONTEXT_MANAGER_H_ */
